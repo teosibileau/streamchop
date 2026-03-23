210cf9f20890204b5e963f7d702292244c977b8b
Teofilo Sibileau <teo.sibileau@gmail.com>
2026-03-23 16:34:11 -0300

    Isolate handling into handler module

--- emitter/src/main.rs
+++ emitter/src/main.rs
@@ -1,10 +1,10 @@
 mod config;
 mod events;
+mod handler;
 mod mqtt;
 
-use chrono::Utc;
 use config::Config;
-use events::{SegmentEvent, SnapshotEvent};
+use handler::Handler;
 use log::{info, warn};
 use mqtt::MqttPublisher;
 use notify::{Event, EventKind, RecursiveMode, Watcher};
@@ -16,6 +16,7 @@ fn main() {
 
     let config = Config::from_env();
     let publisher = MqttPublisher::new(&config);
+    let handler = Handler::new(&config, &publisher);
 
     let (tx, rx) = mpsc::channel::<notify::Result<Event>>();
 
@@ -47,82 +48,10 @@ fn main() {
             };
 
             match ext {
-                "ts" => handle_segment(path, &filename, &config, &publisher),
-                "jpg" => handle_snapshot(path, &filename, &config, &publisher),
+                "ts" => handler.handle_segment(path, &filename),
+                "jpg" => handler.handle_snapshot(path, &filename),
                 _ => {}
             }
         }
     }
-}
-
-fn handle_segment(path: &Path, filename: &str, config: &Config, publisher: &MqttPublisher) {
-    let camera_id = match extract_camera_id(path) {
-        Some(id) => id,
-        None => return,
-    };
-
-    let epoch = match extract_epoch(filename, "segment_") {
-        Some(e) => e,
-        None => return,
-    };
-
-    let base = config.hls_base_url.trim_end_matches('/');
-    let event = SegmentEvent {
-        playlist: format!("{base}/{camera_id}/stream.m3u8"),
-        segment_url: format!("{base}/{camera_id}/{filename}"),
-        camera_id: camera_id.clone(),
-        segment: filename.to_string(),
-        segment_epoch: epoch,
-        timestamp: Utc::now().to_rfc3339(),
-    };
-
-    publisher.publish_segment(&camera_id, &event);
-}
-
-fn handle_snapshot(path: &Path, filename: &str, config: &Config, publisher: &MqttPublisher) {
-    let camera_id = match path
-        .parent()
-        .and_then(|p| p.parent())
-        .and_then(|p| p.file_name())
-        .and_then(|f| f.to_str())
-    {
-        Some(id) => id.to_string(),
-        None => return,
-    };
-
-    let snap_epoch = match extract_epoch(filename, "snap_") {
-        Some(e) => e,
-        None => return,
-    };
-
-    let seg_epoch = snap_epoch - (snap_epoch % config.segment_duration);
-    let segment_name = format!("segment_{seg_epoch}.ts");
-
-    let base = config.hls_base_url.trim_end_matches('/');
-    let event = SnapshotEvent {
-        snapshot_url: format!("{base}/{camera_id}/snapshots/{filename}"),
-        segment_url: format!("{base}/{camera_id}/{segment_name}"),
-        camera_id: camera_id.clone(),
-        snapshot: filename.to_string(),
-        snapshot_epoch: snap_epoch,
-        segment: segment_name,
-        segment_epoch: seg_epoch,
-        timestamp: Utc::now().to_rfc3339(),
-    };
-
-    publisher.publish_snapshot(&camera_id, &event);
-}
-
-fn extract_camera_id(path: &Path) -> Option<String> {
-    path.parent()
-        .and_then(|p| p.file_name())
-        .and_then(|f| f.to_str())
-        .map(|s| s.to_string())
-}
-
-fn extract_epoch(filename: &str, prefix: &str) -> Option<u64> {
-    filename
-        .strip_prefix(prefix)
-        .and_then(|rest| rest.split('.').next())
-        .and_then(|num| num.parse().ok())
 }
